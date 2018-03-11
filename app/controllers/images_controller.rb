e6e7f5c86e18984abe2b86ebc71fd957680bab32
moshimoshinekoko <mizuhotadokoro1@gmail.com>
2018-03-12 07:24:52 +0900

    GOT SABINEKO IMAGES!!

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,14 +2,14 @@ class ImagesController < ApplicationController
   def index
     require 'twitter'
 
-    client = Twitter::REST::Client.new do |config|
-      config.consumer_key         = ""
-      config.consumer_secret      = ""
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key         = "sznhA6doIXhXzj6yEtMJcMnpA"
+      config.consumer_secret      = "8v4RXZDYd3J5DyqduU3WbUh16a3cKt6Kr7qcg2DnpZ176yyLbC"
     end
 
     word = "サビ猫" + " exclude:retweets"
     count = 0
-    client.search(word, :result_type => "recent", :include_entities => true).each do |tweet|
+    @client.search(word, :result_type => "recent", :include_entities => true).each do |tweet|
     tweet.media.each do |media|
     break if count > 9
     puts media.media_url
@@ -17,4 +17,7 @@ class ImagesController < ApplicationController
   end
 end
 end
+
+ helper_method :index
+
 end
